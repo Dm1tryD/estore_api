f863033a19146213abfc3dd9a87fe46687f8bfb2
Dm1tryD <dmitrydanyliukk@gmail.com>
2021-09-14 04:27:19 +0300

    Add shop app

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,7 +75,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "estore_api.users.apps.UsersConfig",
-    # Your stuff: custom apps go here
+    "estore_api.shop",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
