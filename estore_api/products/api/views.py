ada66daf7bf5036975b375ddf30ffde6c16b9e92
Dm1tryD <dmitrydanyliukk@gmail.com>
2021-09-14 10:20:11 +0300

    Create viewsets for notebook and phone models

--- estore_api/products/api/views.py
+++ estore_api/products/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from ..models import Notebook, Phone
+from .serializers import NotebookSerializer, PhoneSerializer
+
+
+class NotebookViewSet(viewsets.ModelViewSet):
+    queryset = Notebook.objects.all()
+    serializer_class = NotebookSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return super(self.__class__, self).get_permissions()
+        return [permissions.IsAdminUser()]
+
+
+class PhoneViewSet(viewsets.ModelViewSet):
+    queryset = Phone.objects.all()
+    serializer_class = PhoneSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return super(self.__class__, self).get_permissions()
+        return [permissions.IsAdminUser()]
