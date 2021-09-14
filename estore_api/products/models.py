44271ce202eb8176a49ff8d1a19d9d628db77304
Dm1tryD <dmitrydanyliukk@gmail.com>
2021-09-14 09:50:11 +0300

    Resolve problems with model performance

--- estore_api/products/models.py
+++ estore_api/products/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-LOW = 0
-NORMAL = 3
-HIGH = 4
+LOW = 1
+NORMAL = 2
+HIGH = 3
 STATUS_CHOICES = (
     (LOW, 'Low'),
     (NORMAL, 'Normal'),
@@ -17,20 +17,23 @@ STORAGE_CHOICES = (
 
 class BaseCharacters(models.Model):
     name = models.CharField(max_length=255)
-    priority = models.CharField(choices=STATUS_CHOICES, max_length=6, default=NORMAL)
+    priority = models.IntegerField(choices=STATUS_CHOICES, default=NORMAL)
 
     class Meta:
         abstract = True
 
 
-class Series(BaseCharacters):
+class Series(models.Model):
+    name = models.CharField(max_length=255)
     year = models.DateField()
 
     def __str__(self):
         return self.name
 
 
-class Category(BaseCharacters):
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
@@ -45,14 +48,22 @@ class Unit(models.Model):
 class Color(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
 
-class Brand(BaseCharacters):
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
 
 class Image(models.Model):
-    images = models.ImageField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.image.name
 
 
 class Product(BaseCharacters):
@@ -62,12 +73,12 @@ class Product(BaseCharacters):
     description = models.CharField(max_length=1000)
     image = models.ManyToManyField(Image, related_name='%(class)s_images')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    series = models.ForeignKey(Series, on_delete=models.CASCADE, blank=True)
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, blank=True)
+    series = models.ForeignKey(Series, on_delete=models.CASCADE, blank=True, null=True)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    discount = models.DecimalField(max_digits=4, decimal_places=2)
+    discount = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
     color = models.ForeignKey(Color, on_delete=models.PROTECT)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='%(class)s_brand')
 
@@ -75,13 +86,18 @@ class Product(BaseCharacters):
         abstract = True
 
     def get_price_with_discount(self):
-        price_with_discount = self.price-((self.price/100)*self.discount)
-        return price_with_discount
+        if self.discount:
+            return self.price-((self.price/100)*self.discount)
+        return self.price
 
 
 class OS(models.Model):
     brand = models.ForeignKey(Brand, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    version = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f"{self.name} {self.version}"
 
 
 class CPU(models.Model):
@@ -89,24 +105,39 @@ class CPU(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class GPU(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Storage(models.Model):
     size = models.PositiveIntegerField()
     type = models.CharField(choices=STORAGE_CHOICES, max_length=3)
 
+    def __str__(self):
+        return f"{self.type} - {self.size} GB"
+
 
 class DisplayType(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class DisplayResolution(models.Model):
     resolution = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.resolution
 
 
 class Display(models.Model):
@@ -114,14 +145,17 @@ class Display(models.Model):
     display_size = models.DecimalField(max_digits=5, decimal_places=2)
     display_resolution = models.ForeignKey(DisplayResolution, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.display_type} {self.display_size} {self.display_resolution}"
+
 
 class ElectronicTech(Product):
     """
     Abstract model for electronics products with a display, cpu, gpu, storage
     """
-    os = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    os = models.ForeignKey(OS, on_delete=models.CASCADE)
     cpu = models.ForeignKey(CPU, on_delete=models.CASCADE, related_name='%(class)s_cpu')
-    gpu = models.ForeignKey(CPU, on_delete=models.CASCADE, related_name='%(class)s_gpu')
+    gpu = models.ForeignKey(GPU, on_delete=models.CASCADE, related_name='%(class)s_gpu')
     storage = models.ForeignKey(Storage, on_delete=models.DO_NOTHING)
     display = models.ForeignKey(Display, on_delete=models.DO_NOTHING)
     front_camera = models.CharField(max_length=50, blank=True)
