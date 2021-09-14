1a0751b7efdde9202f719bf9c7c6e22aa3a6775d
Dm1tryD <dmitrydanyliukk@gmail.com>
2021-09-14 03:04:39 +0300

    Create users models and managers

---
+++ estore_api/users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from .managers import UserManager
+
+
+class User(AbstractUser):
+
+    username = None
+    email = models.EmailField(_('Email address'), unique=True)
+    patronymic = models.CharField(_("Patronymic"), blank=True, max_length=255)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
