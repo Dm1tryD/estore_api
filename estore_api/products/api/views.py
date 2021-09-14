25eb0ebe54a5f599cbb16421d7e349cd887df01f
Dm1tryD <dmitrydanyliukk@gmail.com>
2021-09-14 11:40:29 +0300

    Create mixinviewset and add queryset ordering

--- estore_api/products/api/views.py
+++ estore_api/products/api/views.py
@@ -1,25 +1,25 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from ..models import Notebook, Phone
 from .serializers import NotebookSerializer, PhoneSerializer
 
 
-class NotebookViewSet(viewsets.ModelViewSet):
-    queryset = Notebook.objects.all()
-    serializer_class = NotebookSerializer
-
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return super(self.__class__, self).get_permissions()
-        return [permissions.IsAdminUser()]
+class MixinViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, permissions.DjangoModelPermissionsOrAnonReadOnly,)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_fields = ['category', 'available', 'price', 'cpu', 'gpu']
+    search_fields = ['name', 'brand']
+    ordering_fields = ['priority']
 
 
-class PhoneViewSet(viewsets.ModelViewSet):
+class PhoneViewSet(MixinViewSet):
     queryset = Phone.objects.all()
     serializer_class = PhoneSerializer
 
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return super(self.__class__, self).get_permissions()
-        return [permissions.IsAdminUser()]
+
+class NotebookViewSet(MixinViewSet):
+    queryset = Notebook.objects.all()
+    serializer_class = NotebookSerializer
