0f9b81f6901ba6e923c9687222d072d433b88c42
Dm1tryD <dmitrydanyliukk@gmail.com>
2021-09-14 03:13:08 +0300

    Remove unnecessary imports

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,14 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
-from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
 ]
 
 urlpatterns += [
-    #API
+    # API
     path("api/users/", include(("estore_api.users.urls", "users"), namespace="users")),
 ]
 
