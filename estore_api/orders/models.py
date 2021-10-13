6c2a1457b70577f918c53289d9e12f2775c307f3
Dm1tryD <dmitrydanyliukk@gmail.com>
2021-10-14 02:30:13 +0300

    Create Order and OrderItem model

--- estore_api/orders/models.py
+++ estore_api/orders/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+User = get_user_model()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.DO_NOTHING)
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = GenericForeignKey('content_type', 'object_id')
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.product)
+
+    def get_unit_price(self):
+        return self.product.price
+
+    def get_cost(self):
+        return self.product.price * self.quantity
