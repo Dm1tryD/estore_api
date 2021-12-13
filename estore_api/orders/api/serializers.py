a67a4eee75bf00d41019c0ab2d10c98d13be1dd2
dmitrii.danyliuk <dmitrydanyliukk@gmail.com>
2021-12-13 04:19:28 +0200

    Add name and price discounted fields

--- estore_api/orders/api/serializers.py
+++ estore_api/orders/api/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from ..models import *
 
 
+class OrderItemSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='get_product_name', read_only=True)
+    price_with_discount = serializers.CharField(source='get_unit_price', read_only=True)
+
+    class Meta:
+        fields = ('id', 'order', 'name', 'item_id', 'item_type', 'quantity', 'price_with_discount')
+        model = OrderItem
+
+
 class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
